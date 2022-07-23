4e57428e4344047274ee845282ccaed328c372f6
udav516 <eldar.udav516@yandex.ru>
2022-07-23 14:04:03 +0300

    Task 13

---
+++ Task_13/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int numb = 0;
+
+numb = int.Parse(Console.ReadLine()!);
+
+if (numb / 100 == 0){
+    Console.Write("третьей цифры нет");
+}
+else if (numb / 1000 == 0){
+    Console.WriteLine(numb = numb % 10);
+}
+else
+Console.WriteLine(numb = (numb / 100) % 10);
