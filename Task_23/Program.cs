4748a13f81224a22595b93b08d0d2ab30677b091
udav516 <eldar.udav516@yandex.ru>
2022-07-26 22:03:41 +0300

    Task 23

---
+++ Task_23/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int mas = 0;
+string tir = " ";
+for(int i = 1; i <= num; i++){
+    mas = i * i * i;
+    Console.Write(mas + tir);
+}
