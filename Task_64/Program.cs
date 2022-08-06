64440f55a4ac0f0662e0aa86d5328deb166fa9b1
udav516 <eldar.udav516@yandex.ru>
2022-08-07 01:45:15 +0300

    Task 64

---
+++ Task_64/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 64: Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+int M = 0;
+int N = 0;
+
+int.TryParse(Console.ReadLine(), out M);
+int.TryParse(Console.ReadLine(), out N);
+
+string Function(int M, int N)
+{
+    if (M == N)
+    {
+        return M.ToString();
+    }
+    else
+    {
+        return (M + " " + Function(M + 1, N));
+    }
+}
+Console.WriteLine(Function(M, N));
